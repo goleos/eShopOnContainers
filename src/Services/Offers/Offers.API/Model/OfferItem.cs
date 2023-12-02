74e45eac111a97793debc744a7ec58b80857c677
goleos <goleos4@proton.me>
2023-12-02 11:21:05 +0300

    Create an offer item

---
+++ src/Services/Offers/Offers.API/Model/OfferItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Offers.API.Model
+{
+    public class OfferItem
+    {
+            public int Id { get; set; }
+
+            public string CatalogItemID { get; set; }
+
+            public decimal OfferPrice { get; set; }
+
+}
+}
