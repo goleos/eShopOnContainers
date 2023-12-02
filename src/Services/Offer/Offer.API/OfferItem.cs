b1c8b0f894dc8f23c838d0d756d16bdf864e498d
goleos <goleos4@proton.me>
2023-12-02 14:48:51 +0300

    Add an OfferItem data model

---
+++ src/Services/Offer/Offer.API/OfferItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Offer.API
+{
+    public class OfferItem
+    {
+        public int Id { get; set; }
+
+        public int CatalogItemID { get; set; }
+
+        public decimal OfferPrice { get; set; }
+
+    }
+}
