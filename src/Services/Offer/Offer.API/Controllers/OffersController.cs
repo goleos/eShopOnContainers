bd695c8c4866d42c4de3e6e49837330c104b5c9a
goleos <goleos4@proton.me>
2023-12-03 16:27:21 +0300

    Add simple controller for outputting offers

---
+++ src/Services/Offer/Offer.API/Controllers/OffersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Offer.API.Controllers
+{
+    [ApiController]
+    [Route("offers")]
+    public class OffersController : ControllerBase
+    {
+
+        private readonly OfferItem offer1 = new OfferItem { CatalogItemID = 3, Id = 1, OfferPrice = 4.50M };
+        private readonly OfferItem offer2 = new OfferItem { CatalogItemID = 4, Id = 2, OfferPrice = 6.50M };
+
+        private List<OfferItem> OffersArray = new List<OfferItem>();
+
+        private readonly ILogger<OffersController> _logger;
+
+        public OffersController(ILogger<OffersController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetOffers")]
+        public IEnumerable<OfferItem> Get()
+        {
+            OffersArray.Add(offer1);
+            OffersArray.Add(offer2);
+            return OffersArray;
+        }
+    }
+}
