6cd8558a39dda18fce92b1c5842e7aa79d82b9b8
goleos <goleos4@proton.me>
2023-12-05 16:16:33 +0300

    Create offers controller

---
+++ src/Web/WebMVC/Controllers/OffersController.cs
@@ -0,0 +1,22 @@
+﻿namespace Microsoft.eShopOnContainers.WebMVC.Controllers;
+
+public class OffersController : Controller
+{
+    private IOfferService _offerService;
+
+    public OffersController(IOfferService offerSvc) =>
+        _offerService = offerSvc;
+
+    public async Task<IActionResult> Index()
+    {
+        var offers = await _offerService.GetAllOffers();
+        var vm = new OfferViewModel()
+        {
+            Id = offers[0].Id,
+            CatalogItemID = offers[0].CatalogItemID,
+            OfferPrice = offers[0].OfferPrice,
+        };
+
+        return View(vm);
+    }
+}
