981e7efd779bbd14d2d337dc133effc528317b36
goleos <goleos4@proton.me>
2023-12-05 16:12:21 +0300

    Create a simple offer view and view data

---
+++ src/Web/WebMVC/ViewModels/OfferViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.eShopOnContainers.WebMVC.ViewModels;
+    public record OfferViewModel
+    {
+            public int Id { get; set; }
+
+            public int CatalogItemID { get; set; }
+
+            public decimal OfferPrice { get; set; }
+
+        }
