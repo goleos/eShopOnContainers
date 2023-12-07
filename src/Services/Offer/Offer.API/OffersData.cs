a3e77f5187b2d6eb8d46c2fba53baa70ad174330
goleos <goleos4@proton.me>
2023-12-07 13:43:26 +0300

    Update offers data

--- src/Services/Offer/Offer.API/OffersData.cs
+++ src/Services/Offer/Offer.API/OffersData.cs
@@ -6,9 +6,13 @@
 
         public OffersData() {
             OfferItem offer1 = new OfferItem { CatalogItemID = 2, Id = 1, OfferPrice = 6.50M };
-            OfferItem offer2 = new OfferItem { CatalogItemID = 4, Id = 2, OfferPrice = 6.50M };
+
+            OfferItem offer2 = new OfferItem { CatalogItemID = 4, Id = 2, OfferPrice = 10.00M };
+            OfferItem offer3 = new OfferItem { CatalogItemID = 3, Id = 2, OfferPrice = 5.50M };
+
             OffersArray.Add(offer1);
             OffersArray.Add(offer2);
+            OffersArray.Add(offer3);
         }
 
         public List<OfferItem> GetOfferItems()
